e3797332c552ff84813645576df13a4697639332
Ubuntu <ubuntu@ip-172-31-7-43.ap-south-1.compute.internal>
2022-10-02 02:50:15 +0000

    update settings

--- Blog/settings.py
+++ Blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$-*!p)17(-_kc%o$m6kaqqr2o*k56ih*eecps=pvkhm&lro#fe'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
